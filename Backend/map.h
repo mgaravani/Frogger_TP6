8b8a448150a30c41b0c3163de0117d68aa41723e
Bautista <bmijalovich@itba.edu.ar>
2025-01-03 18:54:46 -0300

    Pasé todo a ingles y termine la funcion initialize_matrix.

--- Backend/map.h
+++ Backend/map.h
@@ -6,7 +6,17 @@
 #define COLUMNS 15
 typedef int8_t map_t[ROWS][COLUMNS];
 
-void print_matrix();
+
+void print_matrix(); // BORRAR DESPUES DE NO USAR MAS (FUN PARA TESTEAT LA MAT)
+
+/*------Funcion initialize_matrix------*/
+/*
+    @brief  Función para inicializar la matriz de juego con spawn aleatorio de ciertos obstáculos y de su largo (troncos, autos, camiones, etc.)
+
+    @param  Fila a desplazar y dirección (derecha 1 e izquierda 0)
+
+    TESTEADA
+*/
 void initialize_matrix();
 
 /*------Funcion shift_row------*/
