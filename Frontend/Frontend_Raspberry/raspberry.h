a69b622c8c573e3d2655f335c63f711c94a8fc40
EmanuelAlbornoz <ealbornoz@itba.edu.ar>
2025-01-28 16:14:30 -0300

    Menu y entrar al juego terminado

--- Frontend/Frontend_Raspberry/raspberry.h
+++ Frontend/Frontend_Raspberry/raspberry.h
@@ -9,11 +9,14 @@
 #include "../../Backend/highscores.h"
 #include "../../Backend/map.h"
 
+#define THRESHOLD 40
+
 void handleJoystickInput(int *choice);
-void playGame();
+int playGame(int choice);
 void ShowFrogger();
 int8_t ShowMenu();
-void initializeFroggerBitmap(uint8_t bitmap[DISP_CANT_Y_DOTS][64]);//inicializa la matriz inicial de mostrar el juego
-void shiftDisplay(uint8_t display[DISP_CANT_Y_DOTS][DISP_CANT_X_DOTS], uint8_t bitmap[DISP_CANT_Y_DOTS][64], int offset);//mueve la matriz mostrada
+void initializeFroggerBitmap(uint8_t bitmap[DISP_CANT_Y_DOTS][46]);//inicializa la matriz inicial de mostrar el juego
+void shiftDisplay(uint8_t display[DISP_CANT_Y_DOTS][DISP_CANT_X_DOTS], uint8_t bitmap[DISP_CANT_Y_DOTS][46], int offset);//mueve la matriz mostrada
+
 
 #endif
